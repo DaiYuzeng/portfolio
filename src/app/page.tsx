c39ea1e4055f42adc139b8b428e7db7e49bb5378
Eric Dai <cridequeger23@gmail.com>
2025-08-11 20:43:41 -0600

    (e2e tests): Set up Playwright e2e tests   - Add minimal home page test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center text-2xl text-slate-600 font-bold tracking-wide">Yuzeng Dai</div>
+    <div data-testid="home-root" className="flex h-screen items-center justify-center text-2xl text-slate-600 font-bold tracking-wide">Yuzeng Dai</div>
   );
 }
