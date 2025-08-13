291ee0804ad9ad09e9891ae86a5053318c7a2a8c
DaiYuzeng <cridequeger23@gmail.com>
2025-08-13 14:17:31 -0600

    (Layout): Finish layout design - Following mobile-first principles, we only wrote CSS for small screens initially. - Added Header and MainSection components to structure the layout. - Updated e2e tests to cover new layout components and behaviors.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import Header from "@/components/Layout/Header";
+import MainSection from "@/components/Layout/MainSection";
+
 export default function Home() {
   return (
-    <div data-testid="home-root" className="flex h-screen items-center justify-center text-2xl text-slate-600 font-bold tracking-wide">Yuzeng Dai</div>
+    <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans">
+      <div>
+        <Header />
+        <main className="pt-24">
+          <MainSection title="About">
+            <p className="mb-4">I’m a developer passionate about building accessible, performance-optimized, and component-driven web applications using React, Vue.js, and modern front-end technologies. </p>
+            <p className="mb-4">Strong focus on design systems, interactive UI components, and scalable architecture.</p>
+            <p className="mb-4">Contributor to open-source projects including Rancher and Harvester.</p>
+          </MainSection>
+          <MainSection title="Experience">
+            <p className="mb-4">I’m a developer passionate about building accessible, performance-optimized, and component-driven web applications using React, Vue.js, and modern front-end technologies. </p>
+            <p className="mb-4">Strong focus on design systems, interactive UI components, and scalable architecture.</p>
+            <p className="mb-4">Contributor to open-source projects including Rancher and Harvester.</p>
+          </MainSection>
+        </main>
+      </div>
+    </div>
   );
 }
---
+++ src/components/Layout/MainSection.tsx
@@ -0,0 +1,14 @@
+import { MainSectionProps } from "@/types/layout"
+
+export default function MainSection( {title, children}: MainSectionProps ) {
+  return (
+    <section>
+      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur">
+        <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200">{title}</h2>
+      </div>
+      <div>
+        {children}
+      </div>
+    </section>
+  )
+}
